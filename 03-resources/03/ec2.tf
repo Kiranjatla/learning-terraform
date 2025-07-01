f7fb494ec16715cd59c3a5f9f935ac5e3324bcd0
KIRAN <ks@gmail.com>
2025-07-01 21:43:00 +0530

    tag name

--- 03-resources/03/ec2.tf
+++ 03-resources/03/ec2.tf
@@ -1,5 +1,11 @@
 resource "aws_instance" "sample" {
-  ami           = "ami-0b4f379183e5706b9"
+  ami                    = "ami-00d48a21603b2119b"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = upper(var.tagName)
+  }
 }
+
+variable "tagName" {}
