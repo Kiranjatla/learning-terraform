b2ba76167fcf76f9114d370612f43a6e687edea8
KIRAN <ks@gmail.com>
2025-07-15 22:01:05 +0530

    Terraform Learning

--- 03-resources/03/ec2.tf
+++ 03-resources/03/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  ami                    = "ami-00d48a21603b2119b"
+  ami                    = "ami-0b4f379183e5706b9"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
